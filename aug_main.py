df52ca1c70128a453da1f04af8ec5a93decd7f4f
fenglang <Feng3133504033@outlook.com>
2023-04-27 13:36:57 +0200

    aug main

--- aug_main.py
+++ aug_main.py
@@ -1,11 +1,17 @@
 import json
 
 from libsa4py.cst_extractor import Extractor
+from libsa4py.cst_transformers import TypeApplier
+from libsa4py.exceptions import ParseError
+from libsa4py.utils import write_file, list_files, read_file
 import utils.pyre as pyre_util
-from utils.utils import list_files, read_file, prep_tar
 import argparse
 import os
 from pathlib import Path
+import time
+from datetime import timedelta
+import libcst as cst
+from tqdm import tqdm
 
 
 # pyre start pipeline
@@ -16,7 +22,8 @@ def pyre_start(project_path):
     pyre_util.start_pyre(project_path)
 
 
-def process_project(project_path, tar_path):
+def process_project(project_path):
+    start_t = time.time()
     project_author = project_path.split("/")[len(project_path.split("/")) - 2]
     project_name = project_path.split("/")[len(project_path.split("/")) - 1]
 
@@ -39,52 +46,52 @@ def process_project(project_path, tar_path):
 
     if len(project_files) != 0:
         print(f'Running pyre query for project {project_path}')
-        for filename, f_relative in project_files:
-            pyre_data_file = pyre_util.pyre_query_types(project_path, filename)
-            # extract types
-            project_analyzed_files[project_id]["src_files"][filename] = \
-                Extractor.extract(read_file(filename), pyre_data_file).to_dict()
-            # extract available types
-            extracted_avl_types = project_analyzed_files[project_id]["src_files"][filename]['imports'] + \
-                                  [c['name'] for c in
-                                   project_analyzed_files[project_id]["src_files"][filename]['classes']]
-
-    print(f'Saving available type hints for {project_id}...')
-    processed_project_dir, avl_types_dir = prep_tar(tar_path)
-
-    if avl_types_dir is not None:
-        if extracted_avl_types:
-            with open(os.path.join(avl_types_dir, f'{project_author}_{project_name}_avltypes.txt'),
-                      'w') as f:
-                for t in extracted_avl_types:
-                    f.write("%s\n" % t)
-
-    if len(project_analyzed_files[project_id]["src_files"].keys()) != 0:
-        project_analyzed_files[project_id]["type_annot_cove"] = \
-            round(sum([project_analyzed_files[project_id]["src_files"][s]["type_annot_cove"] for s in
-                       project_analyzed_files[project_id]["src_files"].keys()]) / len(
-                project_analyzed_files[project_id]["src_files"].keys()), 2)
-
-        processed_file = os.path.join(processed_project_dir, f"{project_author}{project_name}.json")
-        with open(processed_file, 'w') as json_f:
-            json.dump(project_analyzed_files, json_f, indent=4)
+        try:
+            for filename, f_relative in tqdm(project_files):
+                # print(filename)
+                pyre_data_file = pyre_util.pyre_query_types(project_path, filename)
+                # extract types
+                project_analyzed_files[project_id]["src_files"][filename] = \
+                    Extractor.extract(read_file(filename), pyre_data_file).to_dict()
+        except ParseError as err:
+            pass
+        except UnicodeDecodeError:
+            pass
+        except Exception as err:
+            pass
 
     # pyre shutdown
     pyre_util.pyre_server_shutdown(project_path)
+    pyre_util.clean_config(project_path)
+    print(f'inplace augmention for {project_id}...')
+    proj_json = project_analyzed_files
+    for p in proj_json.keys():
+        for i, (f, f_d) in enumerate(proj_json[p]['src_files'].items()):
+            f_read = read_file(f)
+            if len(f_read) != 0:
+                try:
+                    f_parsed = cst.parse_module(f_read)
+                    try:
+                        f_parsed = cst.metadata.MetadataWrapper(f_parsed).visit(TypeApplier(f_d, True))
+                        write_file(f, f_parsed.code)
+                    except KeyError as ke:
+                        pass
+                    except TypeError as te:
+                        pass
+                except cst._exceptions.ParserSyntaxError as pse:
+                    pass
+    print("Finished processing project in %s " % str(timedelta(seconds=time.time()-start_t)))
 
 
 
 def main():
     parser = argparse.ArgumentParser(description='manual to this script')
     parser.add_argument("--p", required=True, type=str, help="Path to Python projects")
-    parser.add_argument("--o", required=True, type=str, help="Path to store JSON-based processed projects")
+    # parser.add_argument("--o", required=false, type=str, help="Path to augmented Python projects")
     args = parser.parse_args()
     project_path = args.p
-    tar_path = args.o
-    process_project(project_path, tar_path)
+    # tar_path = args.o
+    process_project(project_path)
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
